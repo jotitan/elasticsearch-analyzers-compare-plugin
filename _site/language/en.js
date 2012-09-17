c83c06d575e1d149d355b8622f2e75d2a72763e9
jotitan <titanbar@hotmail.com>
2012-09-17 08:00:11 +0200

    Gestion de l'internationalisation

---
+++ _site/language/en.js
@@ -0,0 +1,40 @@
+var languageTabs = {
+   host:"HOST",
+   prefix:"PREFIX",
+   tokenizer:"Tokenizer",
+   filtres:"Filters",
+   "button-open":"Open",
+   "button-close":"Close",
+   "result-value":"Value",
+   "result-score":"Score",
+   "result-index":"Index",
+   "search-type":"Search type",
+   "search-global":"Global",
+   "search-field":"Field",
+   "search-number":"Size",
+   "data-value":"Value",
+   "data-add":"Add entry",
+   "data-add-action":"Add",
+   "data-search":"Search",
+   "data-search-action":"Search",
+   "data-delete":"Delete all datas",
+   "datas":"Datas",
+   "index-generate":"Generate index",
+   "index-delete":"Delete all indexes",
+   "index-add":"Add index",
+   "index-manage":"Manage indexes",
+   "index-created":"Indexes created",
+   "connect":"Connect"
+};
+
+filters.word_delimiter.label = "<span>Word delimiter</span> (split words according to differents rules)";
+filters.asciifolding.label = "<span>Asciifolding</span> (remove accents)";
+filters.elision.label="<span>Elision</span> (remove elision : d'...)";
+filters.stop.label="<span>Stopwords</span> (Words not indexing cause too used in language (in, the, a, an...))";
+filters.snowball.label="<span>Snowball</span> (Find french stem)";
+filters.double_metaphone.label="<span>Double metaphone</span> (phonetic analyse, detects words sounds equals)";
+filters.ngram5.label="<span>nGram 5-10</span> (cut words in parts 5 to 10c)";
+filters.ngram4.label="<span>nGram 4-10</span> (cut words in parts 4 to 10c)";
+filters.stemmer_light.label="<span>Stemmer light french</span> (french stem)";
+filters.stemmer_french.label="<span>Stemmer french</span> (other french stem)";
+filters.stemmer_minimal_french.label="<span>Stemmer minimal french</span> (other french stem)";
